a80a5d3658f40e70443d41c5b0440c03d5a2ed9c
thedrew94 <andrewlaurentiu94@gmail.com>
2025-12-05 17:41:46 +0100

    added docs folder

---
+++ docs/OurStory.tsx
@@ -0,0 +1,29 @@
+export default function OurStory() {
+  return (
+    <div className="introduction" id="introduction">
+      <div className="introduction_bg"></div>
+      <div className="introduction_img">
+        <h6>Tutto integrato in un unico studio</h6>
+      </div>
+      <div className="introduction_description">
+        <h5>Il nostro studio</h5>
+        <div className="introduction_description_main">
+          <span className="decor">“</span>
+          <h6>
+            Il nostro team di Professionisti ha l’obiettivo di distinguersi dagli altri che spesso risultano
+            irreperibili quando il cliente ne ha bisogno.
+            <br />
+            Un problema comune è la difficoltà di capire a chi rivolgersi; <br />
+            – il commercialista, – il consulente del lavoro, – il responsabile della sicurezza sul lavoro generando
+            confusione e frustrazione. <br />
+            <br />
+            Per questo, abbiamo scelto di offrire un approccio innovativo: un unico referente dedicato che gestisce a
+            360° tutte le esigenze aziendali. <br />
+            <br />
+            <span className="text_end">Tutto integrato in un unico studio.</span>
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+}
